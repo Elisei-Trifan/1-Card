e69a3f43398e04b107ee550c1491822a1006a292
Elisei-Trifan <slavaberzan@gmail.com>
2024-10-29 15:25:56 +0200

    add new functions

--- 2-Drag-Drop/app.js
+++ 2-Drag-Drop/app.js
@@ -1,4 +1,5 @@
 const element = document.querySelector('.item')
+const placeholders = document.querySelectorAll('.placeholder')
 
 element.addEventListener('dragstart', dragstart)
 element.addEventListener('dragend', dragend)
@@ -10,4 +11,24 @@ function dragstart(event) {
 
 function dragend(event) {
   event.target.classList.remove('hold', 'hide')
+}
+
+placeholders.forEach((item) => {
+  item.addEventListener('dragover', dragover),
+    item.addEventListener('dragenter', dragenter),
+    item.addEventListener('dragleave', dragleave),
+    item.addEventListener('drop', drop)
+})
+
+function dragover(event) {
+  event.preventDefault()
+}
+function dragenter(event) {
+  event.target.classList.add('hovered')
+}
+function dragleave(event) {
+  event.target.classList.remove('hovered')
+}
+function drop(event) {
+  event.target.append(element)
 }
