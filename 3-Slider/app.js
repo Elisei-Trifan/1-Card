a7f9bf08163a518a23bf4e72cd1b70f292a94368
Elisei-Trifan <slavaberzan@gmail.com>
2024-10-30 23:36:42 +0200

    add function change()

--- 3-Slider/app.js
+++ 3-Slider/app.js
@@ -16,3 +16,25 @@ upBtn.addEventListener('click', () => {
 downBtn.addEventListener('click', () => {
   changeSlide('down')
 })
+
+function changeSlide(direction) {
+  if (direction === 'up') {
+    activeSlideIndex++
+    if (activeSlideIndex === slidesCount) {
+      activeSlideIndex = 0
+    }
+  } else if (direction === 'down') {
+    activeSlideIndex--
+    if (activeSlideIndex < 0) {
+      activeSlideIndex = slidesCount - 1
+    }
+  }
+  const heightContainer = container.clientHeight
+
+  mainSlide.style.transform = `translateY(-${
+    activeSlideIndex * heightContainer
+  }px)`
+  sidebar.style.transform = `translateY(${
+    activeSlideIndex * heightContainer
+  }px)`
+}
