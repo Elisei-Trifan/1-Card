f99aef62cee058a07fe82ef7a19e4fa8fe1f25f8
Elisei-Trifan <slavaberzan@gmail.com>
2024-10-30 23:36:17 +0200

    add new functions

---
+++ 3-Slider/app.js
@@ -0,0 +1,18 @@
+const container = document.querySelector('.container')
+const upBtn = document.querySelector('.up-button')
+const downBtn = document.querySelector('.down-button')
+const sidebar = document.querySelector('.sidebar')
+const mainSlide = document.querySelector('.main-slide')
+const slidesCount = mainSlide.querySelectorAll('div').length
+
+let activeSlideIndex = 0
+
+sidebar.style.top = `-${(slidesCount - 1) * 100}vh`
+
+upBtn.addEventListener('click', () => {
+  changeSlide('up')
+})
+
+downBtn.addEventListener('click', () => {
+  changeSlide('down')
+})
