8c058f35ddfedf5af533bbd2efc73752a327042e
Elisei-Trifan <slavaberzan@gmail.com>
2024-10-31 19:49:39 +0200

    new function removeColor()

--- 4-Board/app.js
+++ 4-Board/app.js
@@ -8,10 +8,17 @@ for (let i = 0; i < SQUARE_NUMBERS; i++) {
   square.addEventListener('mouseover', () => {
     setColor(square)
   })
+  square.addEventListener('mouseleave', () => {
+    removeColor(square)
+  })
 
   board.append(square)
 }
 
 function setColor(element) {
   element.style.background = 'red'
+}
+
+function removeColor(element) {
+  element.style.background = '#1d1d1d'
 }
