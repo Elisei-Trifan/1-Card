f1ff18487f8d8cb6ee7b61d30d3a66fd69226e89
Elisei-Trifan <slavaberzan@gmail.com>
2024-10-31 19:43:52 +0200

    create new function

--- 4-Board/app.js
+++ 4-Board/app.js
@@ -1,9 +1,17 @@
 const board = document.querySelector('#board')
-const SQUARE_NUMBERS = 500
+const SQUARE_NUMBERS = 200
 
-for (let i = 0; i < 500; i++) {
+for (let i = 0; i < SQUARE_NUMBERS; i++) {
   const square = document.createElement('div')
   square.classList.add('square')
 
+  square.addEventListener('mouseover', () => {
+    setColor(square)
+  })
+
   board.append(square)
+}
+
+function setColor(element) {
+  element.style.background = 'red'
 }
