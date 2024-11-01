a095d210839e852722e3575c850f87f9a03cfdc8
Elisei-Trifan <slavaberzan@gmail.com>
2024-11-01 16:50:59 +0200

    add function

--- 5-Game/app.js
+++ 5-Game/app.js
@@ -1,11 +1,12 @@
 const startBtn = document.querySelector('#start')
 
 const screens = document.querySelectorAll('.screen')
-
+const board = document.querySelector('#board')
 const timeList = document.querySelector('#time-list')
 const timeElement = document.querySelector('#time')
 
 let time = 11
+let score = 0
 
 startBtn.addEventListener('click', (event) => {
   event.preventDefault()
@@ -20,23 +21,54 @@ timeList.addEventListener('click', (event) => {
   startGame()
 })
 
+board.addEventListener('click', (event) => {
+  if (event.target.classList.contains('circle')) {
+    score++
+    event.target.remove()
+  }
+})
+
 startGame()
 
 function startGame() {
+  createElement()
   setInterval(decreaseTime, 1000)
   setTime(time)
 }
 
 function decreaseTime() {
-  let current = --time
-  if (current < 10) {
-    current = `0${current}`
+  if (time === 0) {
+    finishGame()
+  } else {
+    let current = --time
+    if (current < 10) {
+      current = `0${current}`
+    }
+    setTime(current)
   }
-  setTime(current)
 }
 
 function setTime(value) {
   timeElement.innerHTML = `00:${value}`
 }
 
-function finishGame
+function finishGame() {}
+
+function createElement() {
+  const size = getRandomNumber(10, 40)
+  const circle = document.createElement('div')
+  const { width, height } = board.getBoundingClientRect()
+
+  const x = getRandomNumber(0, width - size)
+  const y = getRandomNumber(0, height - size)
+  circle.classList.add('circle')
+  circle.style.width = `${size}px`
+  circle.style.height = `${size}px`
+  circle.style.top = `${x}px`
+  circle.style.left = `${y}px`
+  board.append(circle)
+}
+
+function getRandomNumber(min, max) {
+  return Math.round(min + Math.random() * (max - min))
+}
